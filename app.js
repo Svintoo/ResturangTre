c016902479adfe5f57c5a3cf3532903887cb23ed
Sara Johnsson <sara86persson@gmail.com>
2023-12-13 12:25:17 +0100

    added logo

--- app.js
+++ app.js
@@ -1,20 +1,19 @@
 const slider = document.getElementById("rec-food-container");
 let i = 1;
 
-
 setInterval(function () {
-  slide(slider);
+	slide(slider);
 
-  i++;
+	i++;
 }, 3000);
 
 function slide(slider) {
-  if (i <= 2) {
-    slider.scrollLeft += 300;
-    console.log(i);
-  } else {
-    slider.scrollLeft -= 600;
-    i = 0;
-    console.log("reset");
-  }
+	if (i <= 2) {
+		slider.scrollLeft += 300;
+		console.log(i);
+	} else {
+		slider.scrollLeft -= 600;
+		i = 0;
+		console.log("reset");
+	}
 }
